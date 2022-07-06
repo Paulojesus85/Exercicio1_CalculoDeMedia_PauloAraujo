5382a5d5cbe19f0353af6c6915774c638cf1d3b6
Paulojesus85 <paulo.historia.usp@gmail.com>
2022-07-06 10:52:36 -0300

    Atividade concluída

--- Program.cs
+++ Program.cs
@@ -10,6 +10,52 @@ namespace Exercicio1_CalculoDeMedia_PauloAraujo
     {
         static void Main(string[] args)
         {
+            string nome;
+            double trabalho, prova1, prova2, media;
+            int sair;
+
+            do
+            {
+                Console.WriteLine("-----Colégio Unifaat-----");
+                Console.Write("");
+                Console.Write("Informe o nome do aluno: ");
+                nome = Console.ReadLine();
+                Console.Write("Informe a nota do trabalho mensal: ");
+                trabalho = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Informe a nota da primeira prova: ");
+                prova1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Informe a nota da segunda prova: ");
+                prova2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                media = (trabalho + prova1 + prova2) / 3;
+
+                if (media >= 8.5)
+                {
+                    Console.WriteLine($"O aluno {nome} obteve conceito A");
+                }
+                else if (media >= 7)
+                {
+                    Console.WriteLine($"O aluno {nome} obteve conceito B");
+                }
+                else if (media >= 6)
+                {
+                    Console.WriteLine($"O aluno {nome} obteve conceito C");
+                }
+                else if (media >= 0.1)
+                {
+                    Console.WriteLine($"O aluno {nome} obteve conceito D");
+                }
+                else
+                {
+                    Console.WriteLine($"O aluno {nome} obteve conceito E");
+                }
+
+                
+                Console.WriteLine("Digite 1 para CONTINUAR e 2 para SAIR");
+                sair = Convert.ToInt32(Console.ReadLine());
+            } while (sair != 2);
+
         }
     }
 }
